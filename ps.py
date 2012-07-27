3122302ac15f7e901deebc996e582a7844b757a6
fred6 <frexids@gmail.com>
2012-07-27 01:17:11 -0400

    this is bad but i need to sleep. none o dis shit works

--- ps.py
+++ ps.py
@@ -1,6 +1,7 @@
 from lxml import etree
-import os, shutil, errno, subprocess
 from sys import exit, argv
+import os
+from util import *
 
 # get config
 cfg = {}
@@ -11,149 +12,151 @@ for var in list(cfg_ele):
 f.close()
 
 
-#http://nixtu.blogspot.com/2011/11/pandoc-markup-converter.html
-def convert(source, from_format, to_format):
-    # raises OSError if pandoc is not found!
-    # supported formats at http://johnmacfarlane.net/pandoc/
-    p = subprocess.Popen(['pandoc', '--from=' + from_format, '--to=' + to_format, '--mathjax'],
-        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-
-    return p.communicate(bytes(source, 'UTF-8'))[0]
+# set up transforms
+transform = {}
+for tr_name in ['layout', 'index_content', 'page_content']:
+    tpl = etree.parse(cfg['templates_dir'] + tr_name + '.xsl')
+    transform[tr_name] = etree.XSLT(tpl)
 
 
-# http://stackoverflow.com/a/1994840
-def copyanything(src, dst):
-    try:
-        shutil.copytree(src, dst)
-    except OSError as exc: # python >2.5
-        if exc.errno == errno.ENOTDIR:
-            shutil.copy(src, dst)
-        else: raise
-
-def rmanything(thing):
-    try:
-        shutil.rmtree(thing)
-    except OSError as exc: # python >2.5
-        if exc.errno == errno.ENOTDIR:
-            os.remove(thing)
-        else: raise
+def build_etree(data):
+    ele = build_etree_rec(data)
+    return etree.ElementTree(ele)
 
 
-class SiteCompiler():
-    def __init__(self):
-        # each tpl entry is an lxml.etree.ElementTree
-        tpl = {}
-        tpl['layout'] = etree.parse(cfg['templates_dir'] + 'layout.xsl')
-        tpl['page_content'] = etree.parse(cfg['templates_dir'] + 'page_content.xsl')
-        tpl['index_content'] = etree.parse(cfg['templates_dir'] + 'index_content.xsl')
+def build_etree_rec(data):
+    # make a 'root' element out of the top-level dict key
+    listify = list(data.items())
 
-        self.transform = {}
-        self.transform['layout'] = etree.XSLT(tpl['layout'])
-        self.transform['index_content'] = etree.XSLT(tpl['index_content'])
-        self.transform['page_content'] = etree.XSLT(tpl['page_content'])
+    print('@@@@@@@@@@@@@@@@@@@@@@@@@@ IN BUILD_ETREE_REC: LISTIFY @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+    print(listify)
+    print('@@@@@@@@@@@@@@@@@@@@@@@@@@ IN BUILD_ETREE_REC: END LISTIFY @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+    if listify != []:
+        for ele, val in listify:
+            print('@@@@@@@@@@@@@@@@@@@@@@@@@@ IN BUILD_ETREE_REC: inside for ELE/val @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+            print(ele)
+            print(val)
+            print('@@@@@@@@@@@@@@@@@@@@@@@@@@ IN BUILD_ETREE_REC: inside for ELE/val AFTER! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
 
+            ele, val = listify[0]
+            ele_root = etree.Element(ele)
 
-    def _clean_up(self):
-        # make output directory if it doesnt exist
-        if not os.path.exists(cfg['out_dir']):
-            os.makedirs(cfg['out_dir'])
+            if isinstance(val, dict):
+                for e,v in val.items():
+                    subele = build_etree_rec({e: v})
 
-        # clean out old files
-        for f in os.listdir(cfg['out_dir']):
-            rmanything(cfg['out_dir']+f)
+                    if subele is not None:
+                        ele_root.append(subele)
+            elif isinstance(val, str):
+                ele_root.text = val
+            else:
+                # assume list
+                for sub in val:
+                    subele = build_etree_rec(sub)
+                    ele_root.append(subele)
 
+        print('@@@@@@@@@@@@@@@@@@@@@@@@@@ IN BUILD_ETREE_REC: ELE_ROOT @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+        print(etree.tostring(ele_root))
 
-    def runXSLT(self, transform_name, var_tree):
-        return self.transform[transform_name](var_tree)
+        return ele_root
 
-
-    def _build_etree(self, data):
-        ele = self._build_etree_rec(data)
-        return etree.ElementTree(ele)
-
-
-    def _build_etree_rec(self, data):
-        # make a 'root' element out of the top-level dict key
-        ele, val = list(data.items())[0]
-        ele_etree = etree.Element(ele)
-
-        if isinstance(val, dict):
-            subele = self._build_etree_rec(val)
-            ele_etree.append(subele)
-        elif isinstance(val, str):
-            ele_etree.text = val
-        else:
-            # assume list
-            for sub in val:
-                subele = self._build_etree_rec(sub)
-                ele_etree.append(subele)
-
-        return ele_etree
+    else:
+        return None
 
 
+def build_layout_tree(content_tree, page_title):
+    lv = {}
 
-    def _set_layout_vars(self, page_tpl, page_vars, **kwargs):
-        root = etree.Element('root')
-        site_title = etree.SubElement(root, 'site_title')
-        site_title.text = cfg['site_title']
+    if page_title != 'index':
+        lv['page_title'] = page_title
 
-        if kwargs.get('page_name') != None:
-            page_title = etree.SubElement(root, 'page_title')
-            page_title.text = kwargs['page_name']
+    lv['site_title'] = cfg['site_title']
+    lv['content'] = {}
 
-        content = etree.SubElement(root, 'content')
+    tree = build_etree({'root': lv})
 
-        content_tpl_run = self.runXSLT(page_tpl, page_vars)
-        contentroot = content_tpl_run.getroot()
-        contentsub = content.append(content_tpl_run.getroot())
+    if page_title == 'TODO':
+        print('$$$$$$$$$$$$$$$$$$$$$$$$$')
+        print(etree.tostring(tree))
+        print(str(content_tree))
+        print('$$$$$$$$$$$$$$$$$$$$$$$$$')
 
-        self.LV = etree.ElementTree(root)
+    root_ele = tree.getroot()
+    content_ele = root_ele.find('content')
+    content_ele.append(content_tree.getroot())
+    
+    # return ElementTree
+    return etree.ElementTree(root_ele)
 
 
+def write_file_from_dict(tplname, vdict, page_title):
+    if page_title == 'TODO':
+        print('@@@@@@@@@@@@@@@@@@@@@@@@@@ VDICT @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+        print(vdict)
 
-    def _write_file(self, file_name):
-        fname = cfg['out_dir'] + file_name + '.html'
-        f = open(fname, 'w')
-        f.write(str(self.runXSLT('layout', self.LV)))
-        f.close()
+    bev = build_etree(vdict)
 
-    def _compile_page(self, fname):
-        file = open(cfg['site_dir']+fname, 'r')
-        content = file.read()
-        file.close()
+    if page_title == 'TODO':
+        print('@@@@@@@@@@@@@@@@@@@@@@@@@@ BUILD ETREE VDICT @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+        print(etree.tostring(bev))
 
-        pg = {'name': fname[:fname.find(cfg['pages_ext'])],
+    result_tree = transform[tplname](bev)
+
+    if page_title == 'TODO':
+        print('@@@@@@@@@@@@@@@@@@@@@@@@@@ RESULT_TREE @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+        print(str(result_tree))
+
+    final_result_tree = transform['layout'](build_layout_tree(result_tree, page_title))
+
+    if page_title == 'TODO':
+        print('@@@@@@@@@@@@@@@@@@@@@@@@@@ FINAL_RESULT_TREE @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+        print(str(final_result_tree))
+
+    write_file_from_result_tree(page_title, final_result_tree)
+
+
+def write_file_from_result_tree(file_name, result_tree):
+    fname = cfg['out_dir'] + file_name + '.html'
+
+    f = open(fname, 'w')
+    f.write(str(result_tree))
+    f.close()
+
+
+def compile_page(fname):
+    file = open(cfg['site_dir']+fname, 'r')
+    content = file.read()
+    file.close()
+
+    pgvars = {'name': fname[:fname.find(cfg['pages_ext'])],
               'content': bytes.decode(convert(content, 'markdown', 'html'))}
 
-        pgtree = self._build_etree({'page': pg})
+    write_file_from_dict('page_content', pgvars, pgvars['name'])
 
-        self._set_layout_vars('page_content', pgtree, page_name=pg['name'])
-        self._write_file(pg['name'])
-
-        return pg['name']
+    return pgvars['name']
 
 
-    def compile(self):
-        self._clean_up()
-        pages = []
+def compile_index(pages_dict):
+    write_file_from_dict('index_content', pages_dict, 'index')
 
-        # read input directory
-        for ef in os.listdir(cfg['site_dir']):
-            if ef.endswith(cfg['pages_ext']):
-                page_info = self._compile_page(ef)
-                pages.append({'page': {'name': page_info}})
-            else:
-                copyanything(cfg['site_dir']+ef, cfg['out_dir']+ef)
 
-        # compile index file
-        indextree = self._build_etree({'pages': pages})
-        self._set_layout_vars('index_content', indextree)
-        self._write_file('index')
+def compile_site():
+    clean_up(cfg['out_dir'])
+    pages = []
+
+    # read input directory
+    for ef in os.listdir(cfg['site_dir']):
+        if ef.endswith(cfg['pages_ext']):
+            page_info = compile_page(ef)
+            pages.append({'page': {'name': page_info}})
+        else:
+            copyanything(cfg['site_dir']+ef, cfg['out_dir']+ef)
+
+    compile_index({'pages': pages})
 
 
 if __name__ == '__main__':
     if len(argv) == 1:
-        sc = SiteCompiler()
-        sc.compile()
+        compile_site()
     else:
         print("there's no arguments right now.")
---
+++ util.py
@@ -0,0 +1,37 @@
+import os, shutil, errno, subprocess
+
+#http://nixtu.blogspot.com/2011/11/pandoc-markup-converter.html
+def convert(source, from_format, to_format):
+    # raises OSError if pandoc is not found!
+    # supported formats at http://johnmacfarlane.net/pandoc/
+    p = subprocess.Popen(['pandoc', '--from=' + from_format, '--to=' + to_format, '--mathjax'],
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+    return p.communicate(bytes(source, 'UTF-8'))[0]
+
+
+# http://stackoverflow.com/a/1994840
+def copyanything(src, dst):
+    try:
+        shutil.copytree(src, dst)
+    except OSError as exc:
+        if exc.errno == errno.ENOTDIR:
+            shutil.copy(src, dst)
+        else: raise
+
+def rmanything(thing):
+    try:
+        shutil.rmtree(thing)
+    except OSError as exc:
+        if exc.errno == errno.ENOTDIR:
+            os.remove(thing)
+        else: raise
+
+def clean_up(folder):
+    # make output directory if it doesnt exist
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    # clean out old files
+    for f in os.listdir(folder):
+        rmanything(folder+f)
