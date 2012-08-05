ec9edaf5b419a6b76dffeb45f863b4f62053165b
fred6 <frexids@gmail.com>
2012-08-04 22:30:58 -0400

    rip out xslt shit & replace w/ jinja

--- pseudoscience/core.py
+++ pseudoscience/core.py
@@ -1,8 +1,9 @@
-from lxml import etree
 from sys import exit, argv
 from glob import glob
 import os
 from functools import reduce
+
+from jinja2 import Environment, FileSystemLoader
 
 from pseudoscience.util import *
 
@@ -15,69 +16,24 @@ import config as cfg
 def create_transforms():
     global transform
 
-    for tpl in glob(cfg.templates_dir+'*.xsl'):
-        key = tpl.replace(cfg.templates_dir, '').replace('.xsl', '')
-        transform[key] = etree.XSLT(etree.parse(tpl))
+    env = Environment(loader=FileSystemLoader(cfg.templates_dir, encoding='utf-8'))
+    tplext = '.html'
+    for tpl in glob(cfg.templates_dir+'*'+tplext):
+        key = tpl.replace(cfg.templates_dir, '').replace(tplext, '')
+        transform[key] = env.get_template(key+tplext)
 
     
 # chop off the markdown file extension
 pgname_from_fname = lambda f: f[:f.find(cfg.pages_ext)]
 
 
-def build_etree(data):
-    ele = build_etree_rec(data)
-    return etree.ElementTree(ele)
-
-
-def build_etree_rec(data):
-    listify = list(data.items())
-
-    if listify != []:
-        for ele, val in listify:
-            ele, val = listify[0]
-            ele_root = etree.Element(ele)
-
-            if isinstance(val, dict):
-                for e,v in val.items():
-                    subele = build_etree_rec({e: v})
-
-                    if subele is not None:
-                        ele_root.append(subele)
-            elif isinstance(val, str):
-                ele_root.text = val
-            else:
-                # assume list
-                for sub in val:
-                    subele = build_etree_rec(sub)
-                    ele_root.append(subele)
-
-        return ele_root
-
-    else:
-        return None
-
-
-def make_layout_vars(content_tree, page_title, page_path):
-    lv = {}
-    lv['page_title'] = page_title
-    lv['page_path'] = page_path
-    lv['site_title'] = cfg.site_title
-    lv['content'] = {}
-
-    tree = build_etree({'root': lv})
-
-    root_ele = tree.getroot()
-    content_ele = root_ele.find('content')
-    content_ele.append(content_tree.getroot())
-    
-    # return ElementTree
-    return etree.ElementTree(root_ele)
-
-
-def write_file_from_dict(vardict, page_name, folder):
+def render_page(page_vars):
     # if folder matches a select in the render_rules
     # then use whatever templates are specified there
     tplname = cfg.templates['default_page']
+
+    folder = page_vars['path']
+    page_name = page_vars['name']
 
     match_folder = folder+'*'
     match_page = folder+page_name
@@ -86,17 +42,17 @@ def write_file_from_dict(vardict, page_name, folder):
             if cfg.render_rules[match].get('page_template') is not None:
                 tplname = cfg.render_rules[match]['page_template']
 
-    vartree = build_etree(vardict)
-    result_tree = transform[tplname](vartree)
-    LV = make_layout_vars(result_tree, page_name, folder)
-    write_file_from_result_tree(page_name, folder, LV)
+    page_content = transform[tplname].render(page_vars)
+    write_file_from_result_tree(page_name, folder, page_content)
 
 
-def write_file_from_result_tree(file_name, folder, LV):
-    fname = cfg.out_dir + folder + file_name + '.html'
+def write_file_from_result_tree(page_name, folder, content):
+    fname = cfg.out_dir + folder + page_name + '.html'
+
+    layout_vars = {'content': content, 'title': cfg.site_title+' - '+page_name}
 
     f = open(fname, 'w')
-    f.write(str(transform[cfg.templates['default_layout']](LV)))
+    f.write(str(transform[cfg.templates['default_layout']].render(layout_vars)))
     f.close()
 
 
@@ -109,22 +65,40 @@ def compile_page(fname, folder):
           'path': folder,
           'content': bytes.decode(convert(content, cfg.page_format, 'html'))}
 
-    write_file_from_dict({'page': pg}, pg['name'], pg['path'])
+    render_page(pg)
 
 
-def compile_index(pages_dict):
-    write_file_from_dict(pages_dict, 'index', '')
+def compile_index(smap):
+    index_vars = {
+            'name': 'index',
+            'path': '',
+            'map': smap}
+    render_page(index_vars)
 
 
 
 class SiteMap():
     def __init__(self):
         self.smap = {}
+        for o in os.walk(cfg.site_dir):
+            rel_folder = o[0].replace(cfg.site_dir, '')
 
-    def add_to_map(self, path, files):
-        pginfo = lambda f: {'page': {'name': pgname_from_fname(f), 'path': path+'/'}}
-        pages = [pginfo(f) for f in files if self._is_content_file(f)]
-        folder_dict = {'pages': pages, 'subs': []}
+            check =  [f for f in o[2] if self._is_content_file(f)]
+            if check:
+                self.add_to_map(rel_folder, o[1], o[2])
+
+
+    def add_to_map(self, path, folders, files):
+        #pginfo = lambda f: {'page': {'name': pgname_from_fname(f), 'path': path+'/'}}
+        #fldinfo = lambda f: {'folder': {'name': f, 'path': path+'/', 'content': {}}}
+
+        #pages = [pginfo(f) for f in files if self._is_content_file(f)]
+        #folders = [fldinfo(f) for f in folders]
+        pages = [(pgname_from_fname(f), {'title': '', 'path': path+'/'}) 
+                for f in files if self._is_content_file(f)]
+        folders = [(f, {'content': {}, 'path': path+'/'}) 
+                for f in folders]
+        folder_dict = {'pages': dict(pages), 'folders': dict(folders)}
 
         if path == '':
             self.smap = folder_dict
@@ -134,9 +108,8 @@ class SiteMap():
             # the last in path_list is not created yet so only traverse up to second-to-last
             # reduce just turns [a, b, c, d] into self.smap[a][b][c].
             # afterwards we create self.smap[a][b][c][d]
-            parent_folder = reduce(lambda D, k: D['subs'][k], path_list[:-1], self.smap)
-            parent_folder[path_list[-1]] = folder_dict
-
+            parent_folder = reduce(lambda D, k: D['folders'][k], path_list[:-1], self.smap)
+            parent_folder['folders'][path_list[-1]]['content'] = folder_dict 
 
     def _is_content_file(self, filename):
         return filename.endswith(cfg.pages_ext)
@@ -152,7 +125,6 @@ def compile_site():
 
     for o in os.walk(cfg.site_dir):
         rel_folder = o[0].replace(cfg.site_dir, '')
-        smap.add_to_map(rel_folder, o[2])
 
         prep_folder(cfg.out_dir+rel_folder)
         for ef in o[2]:
@@ -162,4 +134,4 @@ def compile_site():
                 copy_to_out(rel_folder+'/'+ef)
 
 
-    compile_index({'root': smap.smap})
+    compile_index(smap.smap)
